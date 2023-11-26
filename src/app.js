81c9fa0410f92a3ce474f20496d3f0ff0dba0017
Wagner Osborne <wgosborne@crimson.ua.edu>
2023-11-26 12:34:04 -0600

    worked with Link and got the Nav Bar working

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Switch, Link, NavLink } from 'react-router-dom';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 
@@ -29,13 +29,33 @@ const HelpPage = () => (
     </div>
 );
 
+const NotFoundPage = () => (
+    <div>
+        404 - <Link to='/' >Go Home</Link>
+    </div>
+);
+
+const Header = () => (
+    <header>
+        <h1>Expensify</h1>
+        <NavLink to='/' activeClassName='is-active' exact={true}>Dashboard</NavLink>
+        <NavLink to='/create' activeClassName='is-active' >Create Expense</NavLink>
+        <NavLink to='/edit' activeClassName='is-active'>Edit Expense</NavLink>
+        <NavLink to='/help' activeClassName='is-active'>Help</NavLink>
+    </header>
+);
+
 const routes = (
     <BrowserRouter>
         <div>
-            <Route path='/' component={ExpenseDashBoardPage} exact={true}/>
-            <Route path='/create' component={AddExpensePage}/>
-            <Route path='/edit' component={EditExpensePage}/>
-            <Route path='/help' component={HelpPage}/>
+            <Header />
+            <Switch>
+                <Route path='/' component={ExpenseDashBoardPage} exact={true}/>
+                <Route path='/create' component={AddExpensePage}/>
+                <Route path='/edit' component={EditExpensePage}/>
+                <Route path='/help' component={HelpPage}/>
+                <Route component={NotFoundPage}/>
+            </Switch>
         </div>
     </BrowserRouter>
 );
