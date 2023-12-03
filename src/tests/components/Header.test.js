03fb25b8602bda247cb0ac9f12f7fd8a72b18bfa
Wagner Osborne <wgosborne@crimson.ua.edu>
2023-12-03 01:34:36 -0600

    right before learning about enzyme and diving into the component testing videos. got rid of the help page navbar link becuase I used update on the test case after learning about snapshot. Now gonn alay my head down and watch the hangover. I will pick up work tomorrow when I am at the library with fiona.

--- src/tests/components/Header.test.js
+++ src/tests/components/Header.test.js
@@ -1,1 +1,14 @@
+import React from 'react';
+import ReactShallowRenderer from 'react-test-renderer/shallow'; //shallow rendering only renders given component, no children
+import Header from '../../components/Header';
+
 //react-test-renderer - allows us to render test components
+//snapshots allow us to track data over time, we use toMatchSnapshot
+
+test('should render Header correctly', () => {
+    const renderer = new ReactShallowRenderer();
+    renderer.render(<Header />);
+    expect(renderer.getRenderOutput()).toMatchSnapshot(); //since its the first time theres no previous so it will pass
+
+    console.log(renderer.getRenderOutput());
+});
