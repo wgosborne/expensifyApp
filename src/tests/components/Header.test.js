6fa9eb53ed286492c0350329688b9452051869a2
Wagner Osborne <wgosborne@crimson.ua.edu>
2023-12-03 12:14:04 -0600

    got the enzyme to json set up and working

--- src/tests/components/Header.test.js
+++ src/tests/components/Header.test.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { ShallowWrapper, mount, shallow } from 'enzyme';
+//import toJSON from 'enzyme-to-json'; removing bc its added in the jest.config.json
 //import ReactShallowRenderer from 'react-test-renderer/shallow'; //shallow rendering only renders given component, no children
 import Header from '../../components/Header';
 
@@ -8,11 +9,12 @@ import Header from '../../components/Header';
 
 test('should render Header correctly', () => {
     const wrapper = shallow(<Header />); //shallow rendering Header
-    expect(wrapper.find('h1').length).toBe(1);
+    expect(wrapper).toMatchSnapshot();
 
+    // expect(toJSON(wrapper)).toMatchSnapshot();
+    //expect(wrapper.find('h1').text()).toBe('Expensify'); // text returns text node
     // const renderer = new ReactShallowRenderer();
     // renderer.render(<Header />);
     // expect(renderer.getRenderOutput()).toMatchSnapshot(); //since its the first time theres no previous so it will pass
-
     // console.log(renderer.getRenderOutput());
 });
