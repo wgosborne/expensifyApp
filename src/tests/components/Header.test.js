5b2f41b6fced3ef7e4f9103a332a11256eae7ca5
Wagner Osborne <wgosborne@crimson.ua.edu>
2023-12-03 12:00:54 -0600

    got the header test working think all the dependencies are right

--- src/tests/components/Header.test.js
+++ src/tests/components/Header.test.js
@@ -1,14 +1,18 @@
 import React from 'react';
-import ReactShallowRenderer from 'react-test-renderer/shallow'; //shallow rendering only renders given component, no children
+import { ShallowWrapper, mount, shallow } from 'enzyme';
+//import ReactShallowRenderer from 'react-test-renderer/shallow'; //shallow rendering only renders given component, no children
 import Header from '../../components/Header';
 
 //react-test-renderer - allows us to render test components
 //snapshots allow us to track data over time, we use toMatchSnapshot
 
 test('should render Header correctly', () => {
-    const renderer = new ReactShallowRenderer();
-    renderer.render(<Header />);
-    expect(renderer.getRenderOutput()).toMatchSnapshot(); //since its the first time theres no previous so it will pass
+    const wrapper = shallow(<Header />); //shallow rendering Header
+    expect(wrapper.find('h1').length).toBe(1);
 
-    console.log(renderer.getRenderOutput());
+    // const renderer = new ReactShallowRenderer();
+    // renderer.render(<Header />);
+    // expect(renderer.getRenderOutput()).toMatchSnapshot(); //since its the first time theres no previous so it will pass
+
+    // console.log(renderer.getRenderOutput());
 });
