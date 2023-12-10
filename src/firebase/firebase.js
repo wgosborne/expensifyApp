6b80614d975ae5a2d30f14adb0b67593b5995b11
Wagner Osborne <wgosborne@crimson.ua.edu>
2023-12-10 01:36:17 -0600

    got firebase set up and got my name set in it

---
+++ src/firebase/firebase.js
@@ -0,0 +1,18 @@
+import * as firebase from 'firebase';
+
+const config = {
+    apiKey: "AIzaSyCi71mX6QeDYiUqZu_QQEnzSigLqh1vOdA",
+    authDomain: "expensify-1c07f.firebaseapp.com",
+    databaseURL: "https://expensify-1c07f-default-rtdb.firebaseio.com",
+    projectId: "expensify-1c07f",
+    storageBucket: "expensify-1c07f.appspot.com",
+    messagingSenderId: "91487209641",
+    appId: "1:91487209641:web:7f51f21ff4576f00b681b8",
+    measurementId: "G-5YVVHW6XYY"
+};
+
+firebase.initializeApp(config);
+
+firebase.database().ref().set({
+    name: 'Wagner Osborne'
+});
