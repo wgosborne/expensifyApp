816b175d574e726b58933a7199e146a9e60d6389
Wagner Osborne <wgosborne@crimson.ua.edu>
2023-12-24 16:22:22 -0600

    set up test database, test cases not up to date, haven't ran, revert to previous push if it doesn't work

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,13 +1,13 @@
 import * as firebase from 'firebase';
 const config = {
-    apiKey: "AIzaSyCi71mX6QeDYiUqZu_QQEnzSigLqh1vOdA",
-    authDomain: "expensify-1c07f.firebaseapp.com",
-    databaseURL: "https://expensify-1c07f-default-rtdb.firebaseio.com",
-    projectId: "expensify-1c07f",
-    storageBucket: "expensify-1c07f.appspot.com",
-    messagingSenderId: "91487209641",
-    appId: "1:91487209641:web:7f51f21ff4576f00b681b8",
-    measurementId: "G-5YVVHW6XYY"
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+    databaseURL: process.env.FIREBASE_DATABASE_URL,
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.FIREBASE_APP_ID,
+    measurementId: process.env.FIREBASE_MEASUREMENT_ID
 };
 
 firebase.initializeApp(config);
--- src/tests/setupTests.js
+++ src/tests/setupTests.js
@@ -1,4 +1,6 @@
 import Enzyme from 'enzyme';
 import Adapter from 'enzyme-adapter-react-15';
 
+require('dotenv').config({ path: '.env.test'});
+
 Enzyme.configure({ adapter: new Adapter() });
